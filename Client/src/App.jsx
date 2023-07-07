d9d00c83d0a0fe76f03563c9db5e319b35ee14c8
mooud <mooudgamer@gmail.com>
2023-07-07 18:57:55 +0330

    update landing page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from "react-router-dom"
 import Editor from "./pages/Editor"
+import Home from "./pages/Home"
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Home/>}/>
       <Route path="/Editor" element={<Editor/>}/>
     </Routes>
   )
