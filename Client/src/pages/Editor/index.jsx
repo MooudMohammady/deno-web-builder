a644278c983fc8987c5b9abae6ae1717130787a6
mooud <mooudgamer@gmail.com>
2023-07-10 15:16:34 +0330

    Merge

--- Client/src/pages/Editor/index.jsx
+++ Client/src/pages/Editor/index.jsx
@@ -23,24 +23,39 @@ const Editor = () => {
       Traits(e);
       e.onReady(() => {
         const btnExp = document.createElement("button");
-        btnExp.innerHTML = "Export";
-        btnExp.className = `px-6 py-1 rounded bg-gray-600 m-1 hover:bg-gray-700`;
+        btnExp.innerHTML = "Export to zip";
+        btnExp.className = `px-6 py-1 rounded border border-gray-600 m-1 hover:bg-gray-500/50`;
         btnExp.type = "button";
-
         e.on("run:export-template", () => {
           const el = e.Modal.getContentEl();
           el?.appendChild(btnExp);
           btnExp.onclick = () => {
             const res = axios.post("http://localhost:4000/postHtml", {
-              data: e.getHtml({ cleanId: true }),
+              data: `
+              <!DOCTYPE html>
+              <html lang="en">
+                <head>
+                  <meta charset="UTF-8" />
+                  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                  <title>Deno Web Builder | Denovin</title>
+                  <script src="https://cdn.tailwindcss.com"></script>
+                  <link rel="stylesheet" href="./aos.css">
+                </head>
+                ${e.getHtml({ cleanId: true })}
+                <script src="./aos.js"></script>
+                <script>
+                  AOS.init();
+                </script>
+              </html>              
+              `,
             });
             const a = document.createElement("a");
             a.style.display = "none";
-            a.download = `${Date.now()}.html`
-            a.href = res.data.href || "http://localhost:4000/files/1342412424.html";
+            a.download = `Deno-web-builder.zip`;
+            a.href = res.data.path;
             document.body.appendChild(a);
             a.click();
-          }
+          };
         });
       });
     }
